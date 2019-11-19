1101e669a2ea8acc384fef77b5dec0c2c517d739
Peter Badenhorst <peter.badenhorst@fantom.foundation>
2019-11-19 20:15:44 +0200

    [Fix] test valid denom

--- x/csdt/tests/app_test.go
+++ x/csdt/tests/app_test.go
@@ -12,7 +12,7 @@ import (
 func TestApp_CreateModifyDeleteCSDT(t *testing.T) {
 	// Setup
 	mapp, keeper := setUpMockAppWithoutGenesis()
-	genAccs, addrs, _, privKeys := mock.CreateGenAccounts(1, cs(c("xrp", 100)))
+	genAccs, addrs, _, privKeys := mock.CreateGenAccounts(1, cs(c("uftm", 100)))
 	testAddr := addrs[0]
 	testPrivKey := privKeys[0]
 	mock.SetGenesis(mapp, genAccs)
@@ -20,9 +20,9 @@ func TestApp_CreateModifyDeleteCSDT(t *testing.T) {
 	header := abci.Header{Height: mapp.LastBlockHeight() + 1}
 	mapp.BeginBlock(abci.RequestBeginBlock{Header: header})
 	ctx := mapp.BaseApp.NewContext(false, header)
-	keeper.GetOracle().AddAsset(ctx, "xrp", "xrp test")
+	keeper.GetOracle().AddAsset(ctx, "uftm", "uftm test")
 	_, _ = keeper.GetOracle().SetPrice(
-		ctx, sdk.AccAddress{}, "xrp",
+		ctx, sdk.AccAddress{}, "uftm",
 		sdk.MustNewDecFromStr("1.00"),
 		sdk.NewInt(10))
 	_ = keeper.GetOracle().SetCurrentPrices(ctx)
@@ -30,20 +30,20 @@ func TestApp_CreateModifyDeleteCSDT(t *testing.T) {
 	mapp.Commit()
 
 	// Create CSDT
-	msgs := []sdk.Msg{types.NewMsgCreateOrModifyCSDT(testAddr, "xrp", i(10), i(5))}
+	msgs := []sdk.Msg{types.NewMsgCreateOrModifyCSDT(testAddr, "uftm", i(10), i(5))}
 	mock.SignCheckDeliver(t, mapp.Cdc, mapp.BaseApp, abci.Header{Height: mapp.LastBlockHeight() + 1}, msgs, []uint64{0}, []uint64{0}, true, true, testPrivKey)
 
-	mock.CheckBalance(t, mapp, testAddr, cs(c(types.StableDenom, 5), c("xrp", 90)))
+	mock.CheckBalance(t, mapp, testAddr, cs(c(types.StableDenom, 5), c("uftm", 90)))
 
 	// Modify CSDT
-	msgs = []sdk.Msg{types.NewMsgCreateOrModifyCSDT(testAddr, "xrp", i(40), i(5))}
+	msgs = []sdk.Msg{types.NewMsgCreateOrModifyCSDT(testAddr, "uftm", i(40), i(5))}
 	mock.SignCheckDeliver(t, mapp.Cdc, mapp.BaseApp, abci.Header{Height: mapp.LastBlockHeight() + 1}, msgs, []uint64{0}, []uint64{1}, true, true, testPrivKey)
 
-	mock.CheckBalance(t, mapp, testAddr, cs(c(types.StableDenom, 10), c("xrp", 50)))
+	mock.CheckBalance(t, mapp, testAddr, cs(c(types.StableDenom, 10), c("uftm", 50)))
 
 	// Delete CSDT
-	msgs = []sdk.Msg{types.NewMsgCreateOrModifyCSDT(testAddr, "xrp", i(-50), i(-10))}
+	msgs = []sdk.Msg{types.NewMsgCreateOrModifyCSDT(testAddr, "uftm", i(-50), i(-10))}
 	mock.SignCheckDeliver(t, mapp.Cdc, mapp.BaseApp, abci.Header{Height: mapp.LastBlockHeight() + 1}, msgs, []uint64{0}, []uint64{2}, true, true, testPrivKey)
 
-	mock.CheckBalance(t, mapp, testAddr, cs(c("xrp", 100)))
+	mock.CheckBalance(t, mapp, testAddr, cs(c("uftm", 100)))
 }
