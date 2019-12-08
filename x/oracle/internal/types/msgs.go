dcf4e30eb90c1e061d794bdcba09482641a5a4ff
Andre Cronje <andre@fantom.foundation>
2019-12-08 19:46:13 +0200

    Commig time.Now() removal
    Signed-off-by: Andre Cronje <andre@fantom.foundation>

--- x/oracle/internal/types/msgs.go
+++ x/oracle/internal/types/msgs.go
@@ -1,7 +1,6 @@
 package types
 
 import (
-	"fmt"
 	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -62,9 +61,6 @@ func (msg MsgPostPrice) ValidateBasic() sdk.Error {
 	}
 	if msg.Price.LT(sdk.ZeroDec()) {
 		return sdk.ErrInternal("invalid (negative) price")
-	}
-	if !msg.Expiry.After(time.Now()) {
-		return sdk.ErrInternal(fmt.Sprintf("invalid (negative) expiry %s now: %s", msg.Expiry.String(), time.Now().String()))
 	}
 	// TODO check coin denoms
 	return nil
