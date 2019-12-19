a845020f533137224817dbde7ee276104ef4c309
Peter Badenhorst <peter.badenhorst@fantom.foundation>
2019-12-19 12:02:48 +0200

    add comments

--- x/csdt/internal/types/interest.go
+++ x/csdt/internal/types/interest.go
@@ -5,6 +5,13 @@ import (
 	"github.com/xar-network/xar-network/x/csdt/internal/keeper"
 )
 
+// InterestRateModel describes the contract needed to calculate the Interest Index.
+// Track the growth of principal of an arbitrary account over time.
+// Using the ratio of that account's interest versus initial principal
+// to calculate the growth of any given account's interest over a subset of that time interval. The
+// interest model contract specifies the simple interest rate at any moment (which, when
+// compounded for each transaction becomes compound interest). We force this interest rate
+// model to be a pure function over the cash, borrows, and reserves of an asset
 type InterestRateModel interface {
 	/**
 	 * @notice Calculates the current borrow interest rate per block
@@ -28,15 +35,22 @@ type InterestRateModel interface {
 	GetSupplyRate(ctx sdk.Context, totalCash sdk.Uint, totalBorrows sdk.Uint, totalReserves sdk.Uint, reserveFactor sdk.Uint) sdk.Uint
 }
 
+// CsdtInterest definition
 type CsdtInterest struct {
 	*keeper.Keeper
 	BorrowRate sdk.Uint
 }
 
+// NewCsdtInterest creates a CSDT Interest model
 func NewCsdtInterest(keeper *keeper.Keeper, borrowRate sdk.Uint) *CsdtInterest {
 	return &CsdtInterest{Keeper: keeper, BorrowRate: borrowRate}
 }
-func CsdtInitialExchangeRate() sdk.Uint { return sdk.NewUint(1) }
+
+// InitialExchangeRate returns the initial exchange rate
+func (CsdtInterest) InitialExchangeRate() sdk.Uint { return sdk.NewUint(1) }
+
+// BlocksPerYear is the approximate number of blocks per year that is assumed by the interest rate model
+func (CsdtInterest) BlocksPerYear() sdk.Uint { return sdk.NewUint(2102400) }
 
 func (ci CsdtInterest) ExchangeRateStored(ctx sdk.Context, totalCash sdk.Uint, totalBorrows sdk.Uint, totalReserves sdk.Uint) sdk.Uint {
 	/*
@@ -51,17 +65,19 @@ func (ci CsdtInterest) ExchangeRateStored(ctx sdk.Context, totalCash sdk.Uint, t
 	*/
 	totalSupply := ci.GetSupply().GetSupply(ctx).GetTotal()
 	if totalSupply.IsZero() {
-		return CsdtInitialExchangeRate()
+		return ci.InitialExchangeRate()
 	}
 
 	totalStableSupplyUint := sdk.NewUintFromBigInt(totalSupply.AmountOf(StableDenom).BigInt())
 	return totalCash.Add(totalBorrows).Sub(totalReserves).Mul(totalStableSupplyUint)
 }
 
+// GetBorrowRate calculates the current borrow interest rate per block
 func (ci CsdtInterest) GetBorrowRate(ctx sdk.Context, totalCash sdk.Uint, totalBorrows sdk.Uint, totalReserves sdk.Uint) sdk.Uint {
 	return ci.BorrowRate
 }
 
+// GetSupplyRate calculates the current supply interest rate per block
 func (ci CsdtInterest) GetSupplyRate(ctx sdk.Context, totalCash sdk.Uint, totalBorrows sdk.Uint, totalReserves sdk.Uint,
 	reserveFactor sdk.Uint) sdk.Uint {
 	// TODO: - change to:
