65d45a99064c29ee9359cc4694c82142d18959d2
Peter Badenhorst <peter.badenhorst@fantom.foundation>
2020-02-14 12:50:30 +0200

    complete interest accrual

--- x/csdt/internal/keeper/interest.go
+++ x/csdt/internal/keeper/interest.go
@@ -1,42 +1,87 @@
 package keeper
 
 import (
+	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/xar-network/xar-network/x/csdt/internal/types"
 )
 
-// AccrueInterest
-func (k Keeper) AccrueInterest(ctx sdk.Context, lastAccruedBlock int64) {
-	// logger := k.Logger(ctx)
-	//
-	// totalCash := sdk.NewInt(0) // k.GetTotalCash(ctx) // TODO: get balance
+func (k Keeper) getAccrualBlockAndIndex(ctx sdk.Context, collateralDenom string) (lastAccrualBlock int64, borrowIndex sdk.Uint) {
+	lastAccrualBlock, ok := k.GetLastAccrualBlock(ctx, collateralDenom)
+	if !ok {
+		panic(fmt.Sprintf("failed to get last accrual block for '%v'", collateralDenom)) // This should exist already
+	}
+
+	borrowIndex, ok = k.GetBorrowIndex(ctx, collateralDenom)
+	if !ok {
+		panic(fmt.Sprintf("failed to get borrow index for '%v'", collateralDenom)) // This should exist already
+	}
+	return
+}
+
+func (k Keeper) getInterestRateModel(ctx sdk.Context, collateralDenom string) types.InterestRateModel {
+	p := k.GetParams(ctx).GetCollateralParam(collateralDenom)
+	return p.InterestModel
+}
+
+func (k Keeper) getTotals(ctx sdk.Context, collateralDenom string) (
+	totalBorrows sdk.Uint, totalCash sdk.Uint, globalReserves sdk.Uint) {
+
+	totalCash, ok := k.GetTotalCash(ctx, collateralDenom)
+	if !ok {
+		panic(fmt.Sprintf("failed to get global cash value for '%v'", collateralDenom)) // This should exist already
+	}
+
+	totalBorrows, ok = k.GetTotalBorrows(ctx, collateralDenom)
+	if !ok {
+		panic(fmt.Sprintf("failed to get global borrow value for '%v'", collateralDenom)) // This should exist already
+	}
+
+	globalReserves, ok = k.GetTotalReserve(ctx, collateralDenom)
+	if !ok {
+		panic(fmt.Sprintf("failed to get global reserve value for '%v'", collateralDenom)) // This should exist already
+	}
+
+	return totalCash, totalBorrows, globalReserves
+}
+
+// AccrueInterest accrues interest and updates the borrow index on every operation.
+// This increases compounding, approaching the true value, regardless of whether the rest of the operation succeeds or not
+func (k Keeper) AccrueInterest(ctx sdk.Context, collateralDenom string, reserveFactor sdk.Uint) {
+	logger := k.Logger(ctx)
+
+	lastAccruedBlock, borrowIndex := k.getAccrualBlockAndIndex(ctx, collateralDenom)
 
 	// get the interest rate (that was in effect since the last update):
-	// ic := types.NewCsdtInterest(sdk.OneUint())
-	// totalBorrows := k.GetGlobalDebt(ctx)
-	// totalReserves := k.GetGlobalReserves(ctx)
-	// borrowRate := ic.GetBorrowRate(ctx, totalCash, totalBorrows, totalReserves)
-	// blockDelta := uint64(ctx.BlockHeight() - lastAccruedBlock)
-	// if blockDelta == 0 {
-	// 	logger.Error("Failed to get accurate block delta")
-	// 	return
-	// }
-	// simpleInterestFactor := borrowRate.MulUint64(blockDelta)
+	ic := k.getInterestRateModel(ctx, collateralDenom)
+	lastTotalSupply, totalBorrows, totalReserves := k.getTotals(ctx, collateralDenom)
+	borrowRate := ic.GetBorrowRate(lastTotalSupply, totalBorrows, totalReserves)
 
-	// update borrowIndex :
+	blockDelta := uint64(ctx.BlockHeight() - lastAccruedBlock)
+	if blockDelta == 0 {
+		logger.Error("Failed to get accurate block delta")
+		return
+	}
+	simpleInterestFactor := borrowRate.MulUint64(blockDelta)
+
+	// update borrowIndex:
 	// borrowIndexNew = borrowIndex × (1 + simpleInterestFactor)
+	borrowIndexNew := borrowIndex.Mul(sdk.OneUint().Add(simpleInterestFactor))
 
 	// calculate the interest accrued:
 	// interestAccumulated = totalBorrows × simpleInterestFactor
+	interestAccumulated := totalBorrows.Mul(simpleInterestFactor)
 
-	// We update borrows and reserves:
+	// update borrows and reserves:
 	// totalBorrowsNew = totalBorrows + interestAccumulated
 	// totalReservesNew = totalReserves + interestAccumulated × reserveFactor
+	totalBorrowsNew := totalBorrows.Add(interestAccumulated)
+	totalReservesNew := totalReserves.Add(interestAccumulated.Mul(reserveFactor))
 
-	// We store the updates back to the blockchain:
-	// Set accrualBlockNumber = getBlockNumber()
-	// Set borrowIndex = borrowIndexNew
-	// Set totalBorrows = totalBorrowsNew
-	// Set totalReserves = totalReservesNew
-
-	k.SetLastAccrualBlock(ctx, ctx.BlockHeight())
+	// store the updates back to the blockchain:
+	k.SetLastAccrualBlock(ctx, ctx.BlockHeight(), collateralDenom)
+	k.SetBorrowIndex(ctx, borrowIndexNew, collateralDenom)
+	k.SetTotalBorrows(ctx, totalBorrowsNew, collateralDenom)
+	k.SetTotalReserve(ctx, totalReservesNew, collateralDenom)
 }
